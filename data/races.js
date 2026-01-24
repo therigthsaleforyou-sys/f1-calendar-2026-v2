520f766a898e167244466073463ae039cb417fc4
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-24 13:23:14 +0000

    Update races.js

--- data/races.js
+++ data/races.js
@@ -1,1 +1,23 @@
-
+const races = [
+  {
+    slug: "australia",
+    name: "Grande Prémio da Austrália",
+    country: "Austrália",
+    hero: "assets/australia.jpg",
+    date: "2026-03-08T05:00:00Z"
+  },
+  {
+    slug: "bahrain",
+    name: "Grande Prémio do Bahrain",
+    country: "Bahrain",
+    hero: "assets/bahrain.jpg",
+    date: "2026-03-15T16:00:00Z"
+  },
+  {
+    slug: "japan",
+    name: "Grande Prémio do Japão",
+    country: "Japão",
+    hero: "assets/japan.jpg",
+    date: "2026-03-29T06:00:00Z"
+  }
+]; 
