b47d9c63312459d413e2b9d7ce234d5a0d6d5c2a
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-25 03:25:08 +0000

    Update races.js

--- data/races.js
+++ data/races.js
@@ -7,5 +7,14 @@ const races = [
     raceDate: "2026-03-08T05:00:00Z",
     image: "assets/races/australia.jpg",
     page: "race-australia.html"
+  },
+  {
+    slug: "bahrain",
+    name: "Grande Prémio do Bahrain",
+    country: "Bahrain",
+    month: "Março",
+    raceDate: "2026-03-15T15:00:00Z",
+    image: "assets/races/bahrain.jpg",
+    page: "race-bahrain.html"
   }
 ];
