588f033d35feb1520ec132a592f34e4bf5db1a2d
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-25 01:28:14 +0000

    Update races.js

--- data/races.js
+++ data/races.js
@@ -3,6 +3,7 @@ const races = [
     slug: "australia",
     name: "Grande Prémio da Austrália",
     raceDate: "2026-03-08T05:00:00Z",
-    image: "assets/races/australia.jpg"
+    image: "assets/races/australia.jpg",
+    page: "race-australia.html"
   }
 ];
