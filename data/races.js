6f654fa13855b39713778d30c1062f7460420329
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-25 14:28:38 +0000

    Update races.js

--- data/races.js
+++ data/races.js
@@ -3,16 +3,32 @@ const races = [
     id: "australia",
     name: "Grande Prémio da Austrália",
     country: "Austrália",
-    circuit: "Albert Park",
-    date: "2026-03-15T06:00:00Z",
+    circuit: "Melbourne",
+    date: "2026-03-08T15:00:00",
     image: "assets/races/australia.jpg"
+  },
+  {
+    id: "china",
+    name: "Grande Prémio da China",
+    country: "China",
+    circuit: "Shanghai",
+    date: "2026-03-15T15:00:00",
+    image: "assets/races/china.jpg"
+  },
+  {
+    id: "japan",
+    name: "Grande Prémio do Japão",
+    country: "Japão",
+    circuit: "Suzuka",
+    date: "2026-03-29T14:00:00",
+    image: "assets/races/japan.jpg"
   },
   {
     id: "bahrain",
     name: "Grande Prémio do Bahrain",
     country: "Bahrain",
     circuit: "Sakhir",
-    date: "2026-03-22T15:00:00Z",
+    date: "2026-04-12T18:00:00",
     image: "assets/races/bahrain.jpg"
   }
 ];
