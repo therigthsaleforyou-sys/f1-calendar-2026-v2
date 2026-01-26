37efa7d2cbff1b1c4b195729571951af8d57e7d8
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-26 11:08:36 +0000

    Update races.js

--- data/races.js
+++ data/races.js
@@ -4,13 +4,37 @@ const races = [
     name: "Grande Prémio da Austrália",
     country: "Austrália",
     fp1: "2026-03-05T03:00:00Z",
-    page: "race-australia.html"
+    page: "race-australia.html",
+    results: [
+      "Max Verstappen",
+      "Lando Norris",
+      "Charles Leclerc",
+      "Carlos Sainz",
+      "Lewis Hamilton",
+      "George Russell",
+      "Oscar Piastri",
+      "Fernando Alonso",
+      "Sergio Perez",
+      "Lance Stroll"
+    ]
   },
   {
     id: "china",
     name: "Grande Prémio da China",
     country: "China",
     fp1: "2026-03-12T04:00:00Z",
-    page: "race-china.html"
+    page: "race-china.html",
+    results: [
+      "Lando Norris",
+      "Oscar Piastri",
+      "Max Verstappen",
+      "Lewis Hamilton",
+      "Charles Leclerc",
+      "George Russell",
+      "Carlos Sainz",
+      "Fernando Alonso",
+      "Sergio Perez",
+      "Lance Stroll"
+    ]
   }
 ];
