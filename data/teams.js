a751a32b57fd0167845fb875895f35fdf28f448e
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-26 17:36:27 +0000

    Update teams.js

--- data/teams.js
+++ data/teams.js
@@ -1,7 +1,12 @@
-const teamsData = [
-  { team: "Red Bull", drivers: ["Max Verstappen", "Sergio Perez"] },
-  { team: "McLaren", drivers: ["Lando Norris", "Oscar Piastri"] },
-  { team: "Ferrari", drivers: ["Charles Leclerc", "Carlos Sainz"] },
-  { team: "Mercedes", drivers: ["Lewis Hamilton", "George Russell"] },
-  { team: "Aston Martin", drivers: ["Fernando Alonso", "Lance Stroll"] }
+window.TEAMS = [
+  { name: "Red Bull Racing", drivers: ["Max Verstappen","Sergio Perez"] },
+  { name: "Mercedes", drivers: ["Lewis Hamilton","George Russell"] },
+  { name: "Ferrari", drivers: ["Charles Leclerc","Carlos Sainz"] },
+  { name: "McLaren", drivers: ["Lando Norris","Oscar Piastri"] },
+  { name: "Alpine", drivers: ["Fernando Alonso","Esteban Ocon"] },
+  { name: "Aston Martin", drivers: ["Lance Stroll","Sebastian Vettel"] },
+  { name: "AlphaTauri", drivers: ["Pierre Gasly","Yuki Tsunoda"] },
+  { name: "Alfa Romeo", drivers: ["Valtteri Bottas","Zhou Guanyu"] },
+  { name: "Haas", drivers: ["Kevin Magnussen","Mick Schumacher"] },
+  { name: "Williams", drivers: ["Nicholas Latifi","Logan Sargeant"] }
 ];
