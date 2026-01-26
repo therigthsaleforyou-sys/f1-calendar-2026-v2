be2a0928d02945b8a198f386c10bc3a3d98f8870
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-26 11:09:41 +0000

    Update teams.js

--- data/teams.js
+++ data/teams.js
@@ -1,7 +1,7 @@
 const teamsData = [
-  { name: "Red Bull", drivers: ["Max Verstappen", "Sergio Pérez"], points: 0 },
-  { name: "Ferrari", drivers: ["Charles Leclerc", "Carlos Sainz"], points: 0 },
-  { name: "McLaren", drivers: ["Lando Norris", "Oscar Piastri"], points: 0 },
-  { name: "Mercedes", drivers: ["Lewis Hamilton", "George Russell"], points: 0 },
-  { name: "Aston Martin", drivers: ["Fernando Alonso", "Lance Stroll"], points: 0 }
+  { team: "Red Bull", drivers: ["Max Verstappen", "Sergio Perez"] },
+  { team: "McLaren", drivers: ["Lando Norris", "Oscar Piastri"] },
+  { team: "Ferrari", drivers: ["Charles Leclerc", "Carlos Sainz"] },
+  { team: "Mercedes", drivers: ["Lewis Hamilton", "George Russell"] },
+  { team: "Aston Martin", drivers: ["Fernando Alonso", "Lance Stroll"] }
 ];
