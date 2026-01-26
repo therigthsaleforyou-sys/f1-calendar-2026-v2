42a6bdc4fbfcbbe868e9f0f5feb2d4fee8696313
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-26 13:25:58 +0000

    Update races.js

--- data/races.js
+++ data/races.js
@@ -1,40 +1,28 @@
-const races = [
+window.RACES = [
   {
     id: "australia",
     name: "Grande Prémio da Austrália",
     country: "Austrália",
-    fp1: "2026-03-05T03:00:00Z",
     page: "race-australia.html",
-    results: [
-      "Max Verstappen",
-      "Lando Norris",
-      "Charles Leclerc",
-      "Carlos Sainz",
-      "Lewis Hamilton",
-      "George Russell",
-      "Oscar Piastri",
-      "Fernando Alonso",
-      "Sergio Perez",
-      "Lance Stroll"
-    ]
+    sessions: {
+      fp1: "2026-03-06T04:00:00Z",
+      fp2: "2026-03-06T08:00:00Z",
+      fp3: "2026-03-07T06:00:00Z",
+      qualifying: "2026-03-07T09:00:00Z",
+      race: "2026-03-08T04:00:00Z"
+    }
   },
   {
     id: "china",
     name: "Grande Prémio da China",
     country: "China",
-    fp1: "2026-03-12T04:00:00Z",
     page: "race-china.html",
-    results: [
-      "Lando Norris",
-      "Oscar Piastri",
-      "Max Verstappen",
-      "Lewis Hamilton",
-      "Charles Leclerc",
-      "George Russell",
-      "Carlos Sainz",
-      "Fernando Alonso",
-      "Sergio Perez",
-      "Lance Stroll"
-    ]
+    sessions: {
+      fp1: "2026-03-13T03:00:00Z",
+      fp2: "2026-03-13T07:00:00Z",
+      fp3: "2026-03-14T03:00:00Z",
+      qualifying: "2026-03-14T07:00:00Z",
+      race: "2026-03-15T07:00:00Z"
+    }
   }
 ];
