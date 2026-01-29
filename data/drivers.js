dfe842e1bb55228619bb3b81938bd486e3538464
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-29 18:18:59 +0000

    Update drivers.js

--- data/drivers.js
+++ data/drivers.js
@@ -1,34 +1,34 @@
 const drivers = [
+  { name: "Lando Norris", team: "McLaren", points: 0 },
+  { name: "Oscar Piastri", team: "McLaren", points: 0 },
+
   { name: "Max Verstappen", team: "Red Bull Racing", points: 0 },
-  { name: "Sergio Pérez", team: "Red Bull Racing", points: 0 },
+  { name: "Isack Hadjar", team: "Red Bull Racing", points: 0 },
 
   { name: "Charles Leclerc", team: "Ferrari", points: 0 },
   { name: "Lewis Hamilton", team: "Ferrari", points: 0 },
 
-  { name: "George Russell", team: "Mercedes", points: 0 },
-  { name: "Andrea Kimi Antonelli", team: "Mercedes", points: 0 },
+  { name: "Alex Albon", team: "Williams", points: 0 },
+  { name: "Carlos Sainz", team: "Williams", points: 0 },
 
-  { name: "Lando Norris", team: "McLaren", points: 0 },
-  { name: "Oscar Piastri", team: "McLaren", points: 0 },
+  { name: "Liam Lawson", team: "Racing Bulls", points: 0 },
+  { name: "Arvid Lindblad", team: "Racing Bulls", points: 0 },
 
   { name: "Fernando Alonso", team: "Aston Martin", points: 0 },
   { name: "Lance Stroll", team: "Aston Martin", points: 0 },
 
-  { name: "Esteban Ocon", team: "Alpine", points: 0 },
+  { name: "Esteban Ocon", team: "Haas", points: 0 },
+  { name: "Oliver Bearman", team: "Haas", points: 0 },
+
+  { name: "Nico Hülkenberg", team: "Audi", points: 0 },
+  { name: "Gabriel Bortoleto", team: "Audi", points: 0 },
+
   { name: "Pierre Gasly", team: "Alpine", points: 0 },
+  { name: "Franco Colapinto", team: "Alpine", points: 0 },
 
-  { name: "Alexander Albon", team: "Williams", points: 0 },
-  { name: "Logan Sargeant", team: "Williams", points: 0 },
+  { name: "George Russell", team: "Mercedes", points: 0 },
+  { name: "Andrea Kimi Antonelli", team: "Mercedes", points: 0 },
 
-  { name: "Yuki Tsunoda", team: "RB", points: 0 },
-  { name: "Daniel Ricciardo", team: "RB", points: 0 },
-
-  { name: "Valtteri Bottas", team: "Kick Sauber", points: 0 },
-  { name: "Zhou Guanyu", team: "Kick Sauber", points: 0 },
-
-  { name: "Kevin Magnussen", team: "Haas", points: 0 },
-  { name: "Nico Hülkenberg", team: "Haas", points: 0 },
-
-  { name: "Piloto Andretti 1", team: "Andretti Cadillac", points: 0 },
-  { name: "Piloto Andretti 2", team: "Andretti Cadillac", points: 0 }
+  { name: "Sergio Pérez", team: "Cadillac", points: 0 },
+  { name: "Valtteri Bottas", team: "Cadillac", points: 0 }
 ];
