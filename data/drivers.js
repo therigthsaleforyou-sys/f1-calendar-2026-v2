a6885896ad09cb527e30c2baaf80d8777620eac0
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-29 18:15:58 +0000

    Create drivers.js

---
+++ data/drivers.js
@@ -0,0 +1,34 @@
+const drivers = [
+  { name: "Max Verstappen", team: "Red Bull Racing", points: 0 },
+  { name: "Sergio Pérez", team: "Red Bull Racing", points: 0 },
+
+  { name: "Charles Leclerc", team: "Ferrari", points: 0 },
+  { name: "Lewis Hamilton", team: "Ferrari", points: 0 },
+
+  { name: "George Russell", team: "Mercedes", points: 0 },
+  { name: "Andrea Kimi Antonelli", team: "Mercedes", points: 0 },
+
+  { name: "Lando Norris", team: "McLaren", points: 0 },
+  { name: "Oscar Piastri", team: "McLaren", points: 0 },
+
+  { name: "Fernando Alonso", team: "Aston Martin", points: 0 },
+  { name: "Lance Stroll", team: "Aston Martin", points: 0 },
+
+  { name: "Esteban Ocon", team: "Alpine", points: 0 },
+  { name: "Pierre Gasly", team: "Alpine", points: 0 },
+
+  { name: "Alexander Albon", team: "Williams", points: 0 },
+  { name: "Logan Sargeant", team: "Williams", points: 0 },
+
+  { name: "Yuki Tsunoda", team: "RB", points: 0 },
+  { name: "Daniel Ricciardo", team: "RB", points: 0 },
+
+  { name: "Valtteri Bottas", team: "Kick Sauber", points: 0 },
+  { name: "Zhou Guanyu", team: "Kick Sauber", points: 0 },
+
+  { name: "Kevin Magnussen", team: "Haas", points: 0 },
+  { name: "Nico Hülkenberg", team: "Haas", points: 0 },
+
+  { name: "Piloto Andretti 1", team: "Andretti Cadillac", points: 0 },
+  { name: "Piloto Andretti 2", team: "Andretti Cadillac", points: 0 }
+];
