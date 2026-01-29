5dddc48c828047530450729a8e12b8b26f92aa04
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-29 18:18:35 +0000

    Update teams.js

--- data/teams.js
+++ data/teams.js
@@ -1,13 +1,13 @@
 const teams = [
+  { name: "McLaren", points: 0 },
   { name: "Red Bull Racing", points: 0 },
   { name: "Ferrari", points: 0 },
-  { name: "Mercedes", points: 0 },
-  { name: "McLaren", points: 0 },
-  { name: "Aston Martin", points: 0 },
-  { name: "Alpine", points: 0 },
   { name: "Williams", points: 0 },
-  { name: "RB", points: 0 },
-  { name: "Kick Sauber", points: 0 },
+  { name: "Racing Bulls", points: 0 },
+  { name: "Aston Martin", points: 0 },
   { name: "Haas", points: 0 },
-  { name: "Andretti Cadillac", points: 0 }
+  { name: "Audi", points: 0 },
+  { name: "Alpine", points: 0 },
+  { name: "Mercedes", points: 0 },
+  { name: "Cadillac", points: 0 }
 ];
