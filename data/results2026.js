ce468f38e8854c3b10be55efaabc02e60484c359
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 01:20:07 +0000

    Create results2026.js

---
+++ data/results2026.js
@@ -0,0 +1,5 @@
+const results2026 = {
+  australia: { pole: null, fastestLap: null, podium: [], points: {} },
+  bahrain: { pole: null, fastestLap: null, podium: [], points: {} }
+  // Adicionar restantes corridas 2026
+};
