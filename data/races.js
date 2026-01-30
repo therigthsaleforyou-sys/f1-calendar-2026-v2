d01a90657fa0601bd700371d74b329dc5dd8765a
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 00:23:26 +0000

    Update races.js

--- data/races.js
+++ data/races.js
@@ -2,11 +2,13 @@ const races = [
   {
     name: "Grande Prémio da Austrália",
     location: "Melbourne",
-    date: "2026-03-08T04:00:00"
-  },
-  {
-    name: "Grande Prémio do Bahrain",
-    location: "Sakhir",
-    date: "2026-03-15T16:00:00"
+    datetime: "2026-03-08T04:00:00",
+    image: "assets/races/australia.jpg",
+    sessions: {
+      fp1: "02:30",
+      fp2: "06:00",
+      quali: "05:00",
+      race: "04:00"
+    }
   }
 ];
