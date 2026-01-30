77d49379adde5dd0365984a2918f06e6d58caa3f
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 11:41:41 +0000

    Update results2025.js

--- data/results2025.js
+++ data/results2025.js
@@ -1,98 +1,10 @@
 const results2025 = {
-  "australia": {
+  australia: {
     pole: "Lando Norris",
     poleTime: "1:15.096",
     fastestLap: "Lando Norris",
     fastestLapTime: "1:22.167",
-    podium: "Lando Norris, Max Verstappen, George Russell",
-    meteo: "Sol"
-  },
-  "china": {
-    pole: "Oscar Piastri",
-    poleTime: "1:30.641",
-    fastestLap: "Lando Norris",
-    fastestLapTime: "1:35.454",
-    podium: "Oscar Piastri, Lando Norris, George Russell",
-    meteo: "Sol"
-  },
-  "japan": {
-    pole: "Max Verstappen",
-    poleTime: "1:26.983",
-    fastestLap: "Andrea Kimi Antonelli",
-    fastestLapTime: "1:30.965",
-    podium: "Max Verstappen, Oscar Piastri, Lando Norris",
-    meteo: "Sol"
-  },
-  "bahrain": {
-    pole: "Oscar Piastri",
-    poleTime: "1:29.841",
-    fastestLap: "Oscar Piastri",
-    fastestLapTime: "1:35.140",
-    podium: "Oscar Piastri, George Russell, Lando Norris",
-    meteo: "Sol"
-  },
-  "saudiarabia": {
-    pole: "Max Verstappen",
-    poleTime: "1:27.294",
-    fastestLap: "Lando Norris",
-    fastestLapTime: "1:31.778",
-    podium: "Oscar Piastri, Max Verstappen, Charles Leclerc",
-    meteo: "Sol"
-  },
-  "miami": {
-    pole: "Max Verstappen",
-    poleTime: "1:26.204",
-    fastestLap: "Lando Norris",
-    fastestLapTime: "1:29.746",
-    podium: "Oscar Piastri, Lando Norris, George Russell",
-    meteo: "Sol"
-  },
-  "emiliaromagna": {
-    pole: "Oscar Piastri",
-    poleTime: "1:14.670",
-    fastestLap: "Max Verstappen",
-    fastestLapTime: "1:17.988",
-    podium: "Max Verstappen, Lando Norris, Oscar Piastri",
-    meteo: "Sol"
-  },
-  "monaco": {
-    pole: "Lando Norris",
-    poleTime: "1:09.954",
-    fastestLap: "Lando Norris",
-    fastestLapTime: "1:13.221",
-    podium: "Lando Norris, Oscar Piastri, Max Verstappen",
-    meteo: "Sol"
-  },
-  "spain": {
-    pole: "Oscar Piastri",
-    poleTime: "1:11.546",
-    fastestLap: "Oscar Piastri",
-    fastestLapTime: "1:15.743",
-    podium: "Oscar Piastri, Lando Norris, George Russell",
-    meteo: "Sol"
-  },
-  "canada": {
-    pole: "George Russell",
-    poleTime: "1:10.899",
-    fastestLap: "George Russell",
-    fastestLapTime: "1:14.119",
-    podium: "George Russell, Max Verstappen, Oscar Piastri",
-    meteo: "Sol"
-  },
-  "austria": {
-    pole: "Lando Norris",
-    poleTime: "1:03.971",
-    fastestLap: "Oscar Piastri",
-    fastestLapTime: "1:07.924",
-    podium: "Lando Norris, Oscar Piastri, Max Verstappen",
-    meteo: "Sol"
-  },
-  "greatbritain": {
-    pole: "Max Verstappen",
-    poleTime: "1:24.892",
-    fastestLap: "Oscar Piastri",
-    fastestLapTime: "1:29.337",
-    podium: "Max Verstappen, Oscar Piastri, Lando Norris",
+    podium: "Norris · Verstappen · Russell",
     meteo: "Sol"
   }
 };
