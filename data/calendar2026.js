ebfc04250db77aa21def15b91048de78fd4a2344
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 14:21:48 +0000

    Update calendar2026.js

--- data/calendar2026.js
+++ data/calendar2026.js
@@ -1,15 +1,1 @@
-const calendar2026 = [
-  {
-    id: "australia",
-    name: "Grande Prémio da Austrália",
-    image: "assets/races/australia.jpg",
-    sessions: {
-      FP1: "06/03/2026 02:30",
-      FP2: "06/03/2026 06:00",
-      FP3: "07/03/2026 02:30",
-      Qualificação: "07/03/2026 06:00",
-      Corrida: "08/03/2026 04:00"
-    },
-    raceDateISO: "2026-03-08T04:00:00"
-  }
-];
+const nextRaceDate = "2026-03-08T04:00:00Z";
