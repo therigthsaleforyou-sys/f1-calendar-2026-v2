97e3808a6ca9e5df3a880a00daa7a58cf46c8644
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 00:24:00 +0000

    Update drivers.js

--- data/drivers.js
+++ data/drivers.js
@@ -1,22 +1,5 @@
 const drivers = [
+  { name: "Max Verstappen", team: "Red Bull", points: 0 },
   { name: "Lando Norris", team: "McLaren", points: 0 },
-  { name: "Oscar Piastri", team: "McLaren", points: 0 },
-
-  { name: "Max Verstappen", team: "Red Bull Racing", points: 0 },
-  { name: "Isack Hadjar", team: "Red Bull Racing", points: 0 },
-
-  { name: "Charles Leclerc", team: "Ferrari", points: 0 },
-  { name: "Lewis Hamilton", team: "Ferrari", points: 0 },
-
-  { name: "Alex Albon", team: "Williams", points: 0 },
-  { name: "Carlos Sainz", team: "Williams", points: 0 },
-
-  { name: "Fernando Alonso", team: "Aston Martin", points: 0 },
-  { name: "Lance Stroll", team: "Aston Martin", points: 0 },
-
-  { name: "George Russell", team: "Mercedes", points: 0 },
-  { name: "Andrea Kimi Antonelli", team: "Mercedes", points: 0 },
-
-  { name: "Sergio Pérez", team: "Cadillac", points: 0 },
-  { name: "Valtteri Bottas", team: "Cadillac", points: 0 }
+  { name: "Charles Leclerc", team: "Ferrari", points: 0 }
 ];
