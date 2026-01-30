a26aa31428f99bcf7db336d609d8c56c43de423f
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 01:28:05 +0000

    Create calendar2026.js

---
+++ data/calendar2026.js
@@ -0,0 +1,132 @@
+const calendar2026 = [
+  {
+    id: "australia",
+    name: "Grande Prémio da Austrália",
+    circuit: "Melbourne",
+    image: "assets/races/australia.jpg",
+    sessions: {
+      FP1: "2026-03-06T02:30:00",
+      FP2: "2026-03-06T06:00:00",
+      FP3: "2026-03-07T02:30:00",
+      Qualifying: "2026-03-07T06:00:00",
+      Race: "2026-03-08T04:00:00"
+    }
+  },
+  {
+    id: "china",
+    name: "Grande Prémio da China",
+    circuit: "Shanghai",
+    image: "assets/races/china.jpg",
+    sessions: {
+      FP1: "2026-03-13T03:30:00",
+      SprintQuali: "2026-03-13T07:30:00",
+      Sprint: "2026-03-14T03:00:00",
+      Qualifying: "2026-03-14T07:00:00",
+      Race: "2026-03-15T07:00:00"
+    }
+  },
+  {
+    id: "japan",
+    name: "Grande Prémio do Japão",
+    circuit: "Suzuka",
+    image: "assets/races/japan.jpg",
+    sessions: {
+      FP1: "2026-03-27T02:30:00",
+      FP2: "2026-03-27T06:00:00",
+      FP3: "2026-03-28T02:30:00",
+      Qualifying: "2026-03-28T06:00:00",
+      Race: "2026-03-29T05:00:00"
+    }
+  },
+  {
+    id: "bahrain",
+    name: "Grande Prémio do Bahrain",
+    circuit: "Sakhir",
+    image: "assets/races/bahrain.jpg",
+    sessions: {
+      FP1: "2026-04-10T11:30:00",
+      FP2: "2026-04-10T15:00:00",
+      FP3: "2026-04-11T12:30:00",
+      Qualifying: "2026-04-11T16:00:00",
+      Race: "2026-04-12T15:00:00"
+    }
+  },
+  {
+    id: "saudiarabia",
+    name: "Grande Prémio da Arábia Saudita",
+    circuit: "Jeddah",
+    image: "assets/races/saudiarabia.jpg",
+    sessions: {
+      FP1: "2026-04-17T16:30:00",
+      FP2: "2026-04-17T20:00:00",
+      FP3: "2026-04-18T16:30:00",
+      Qualifying: "2026-04-18T20:00:00",
+      Race: "2026-04-19T20:00:00"
+    }
+  },
+  {
+    id: "miami",
+    name: "Grande Prémio de Miami",
+    circuit: "Miami",
+    image: "assets/races/miami.jpg",
+    sessions: {
+      FP1: "2026-05-01T12:30:00",
+      SprintQuali: "2026-05-01T16:30:00",
+      Sprint: "2026-05-02T12:00:00",
+      Qualifying: "2026-05-02T16:00:00",
+      Race: "2026-05-03T16:00:00"
+    }
+  },
+  {
+    id: "canada",
+    name: "Grande Prémio do Canadá",
+    circuit: "Montreal",
+    image: "assets/races/canada.jpg",
+    sessions: {
+      FP1: "2026-05-22T12:30:00",
+      SprintQuali: "2026-05-22T16:30:00",
+      Sprint: "2026-05-23T12:00:00",
+      Qualifying: "2026-05-23T16:00:00",
+      Race: "2026-05-24T16:00:00"
+    }
+  },
+  {
+    id: "monaco",
+    name: "Grande Prémio de Mónaco",
+    circuit: "Monaco",
+    image: "assets/races/monaco.jpg",
+    sessions: {
+      FP1: "2026-06-05T13:30:00",
+      FP2: "2026-06-05T17:00:00",
+      FP3: "2026-06-06T12:30:00",
+      Qualifying: "2026-06-06T16:00:00",
+      Race: "2026-06-07T15:00:00"
+    }
+  },
+  {
+    id: "barcelona",
+    name: "Grande Prémio de Espanha",
+    circuit: "Barcelona-Catalunya",
+    image: "assets/races/barcelona.jpg",
+    sessions: {
+      FP1: "2026-06-12T13:30:00",
+      FP2: "2026-06-12T17:00:00",
+      FP3: "2026-06-13T12:30:00",
+      Qualifying: "2026-06-13T16:00:00",
+      Race: "2026-06-14T15:00:00"
+    }
+  },
+  {
+    id: "austria",
+    name: "Grande Prémio da Áustria",
+    circuit: "Spielberg",
+    image: "assets/races/austria.jpg",
+    sessions: {
+      FP1: "2026-06-26T13:30:00",
+      FP2: "2026-06-26T17:00:00",
+      FP3: "2026-06-27T12:30:00",
+      Qualifying: "2026-06-27T16:00:00",
+      Race: "2026-06-28T15:00:00"
+    }
+  }
+];
