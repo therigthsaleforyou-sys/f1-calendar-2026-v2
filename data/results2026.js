b448ce3a5b75dec023fb869b7d01df98543bc4ea
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 11:53:46 +0000

    Delete data/results2026.js

--- data/results2026.js
+++
@@ -1,5 +0,0 @@
-const results2026 = {
-  australia: { pole: null, fastestLap: null, podium: [], points: {} },
-  bahrain: { pole: null, fastestLap: null, podium: [], points: {} }
-  // Adicionar restantes corridas 2026
-};
