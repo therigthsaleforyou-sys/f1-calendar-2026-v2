9f1d068d5b08e4b0570f19545837eec1f0c02eff
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 15:01:26 +0000

    Update results2025.js

--- data/results2025.js
+++ data/results2025.js
@@ -1,10 +1,9 @@
 const results2025 = {
   australia: {
-    pole: "Lando Norris",
-    poleTime: "1:15.096",
-    fastestLap: "Lando Norris",
-    fastestLapTime: "1:22.167",
-    podium: "Norris · Verstappen · Russell",
-    meteo: "Sol"
+    podium: [
+      { pos: 1, driver: "Max Verstappen", time: "1:20:15.123" },
+      { pos: 2, driver: "Charles Leclerc", time: "+5.412s" },
+      { pos: 3, driver: "Lando Norris", time: "+9.876s" }
+    ]
   }
 };
