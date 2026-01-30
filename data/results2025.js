7e805a80e11be693c0415b2477534ccb415b12c8
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 02:05:15 +0000

    Update results2025.js

--- data/results2025.js
+++ data/results2025.js
@@ -1,13 +1,17 @@
 const results2025 = {
   "australia": {
     pole: "Max Verstappen",
+    poleTime: "1:14.200",
     fastestLap: "Lewis Hamilton",
+    fastestLapTime: "1:18.342",
     podium: "Max Verstappen, Lewis Hamilton, Lando Norris",
     meteo: "Sol, 22°C"
   },
   "bahrain": {
     pole: "Charles Leclerc",
+    poleTime: "1:30.256",
     fastestLap: "Sergio Perez",
+    fastestLapTime: "1:34.842",
     podium: "Charles Leclerc, Sergio Perez, George Russell",
     meteo: "Sol, 28°C"
   }
