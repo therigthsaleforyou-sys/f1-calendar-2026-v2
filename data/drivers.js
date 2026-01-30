0b2149194793cc38cb5110dbc2442ea26e5984bb
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 01:06:03 +0000

    Update drivers.js

--- data/drivers.js
+++ data/drivers.js
@@ -1,45 +1,24 @@
 const drivers = [
-  // Red Bull
   { name: "Max Verstappen", team: "redbull", points: 0 },
   { name: "Sergio Pérez", team: "redbull", points: 0 },
-
-  // Ferrari
   { name: "Charles Leclerc", team: "ferrari", points: 0 },
-  { name: "Lewis Hamilton", team: "ferrari", points: 0 },
-
-  // McLaren
+  { name: "Carlos Sainz", team: "ferrari", points: 0 },
   { name: "Lando Norris", team: "mclaren", points: 0 },
   { name: "Oscar Piastri", team: "mclaren", points: 0 },
-
-  // Mercedes
+  { name: "Lewis Hamilton", team: "mercedes", points: 0 },
   { name: "George Russell", team: "mercedes", points: 0 },
-  { name: "Andrea Kimi Antonelli", team: "mercedes", points: 0 },
-
-  // Aston Martin
   { name: "Fernando Alonso", team: "astonmartin", points: 0 },
   { name: "Lance Stroll", team: "astonmartin", points: 0 },
-
-  // Alpine
   { name: "Pierre Gasly", team: "alpine", points: 0 },
   { name: "Esteban Ocon", team: "alpine", points: 0 },
-
-  // Williams
   { name: "Alex Albon", team: "williams", points: 0 },
-  { name: "Carlos Sainz", team: "williams", points: 0 },
-
-  // Kick Sauber
+  { name: "Logan Sargeant", team: "williams", points: 0 },
   { name: "Valtteri Bottas", team: "sauber", points: 0 },
   { name: "Zhou Guanyu", team: "sauber", points: 0 },
-
-  // RB
-  { name: "Yuki Tsunoda", team: "rb", points: 0 },
-  { name: "Daniel Ricciardo", team: "rb", points: 0 },
-
-  // Haas
-  { name: "Oliver Bearman", team: "haas", points: 0 },
   { name: "Kevin Magnussen", team: "haas", points: 0 },
-
-  // Cadillac
-  { name: "Sergio Pérez", team: "cadillac", points: 0 },
-  { name: "Valtteri Bottas", team: "cadillac", points: 0 }
+  { name: "Oliver Bearman", team: "haas", points: 0 },
+  { name: "Yuki Tsunoda", team: "alphaTauri", points: 0 },
+  { name: "Daniel Ricciardo", team: "alphaTauri", points: 0 },
+  { name: "Guanyu Zhou", team: "alfaRomeo", points: 0 },
+  { name: "Valtteri Bottas", team: "alfaRomeo", points: 0 }
 ];
