02b4699ddb2817f05c715de03193555442f88d76
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-01-30 11:41:05 +0000

    Update calendar2026.js

--- data/calendar2026.js
+++ data/calendar2026.js
@@ -2,28 +2,14 @@ const calendar2026 = [
   {
     id: "australia",
     name: "Grande Prémio da Austrália",
-    circuit: "Melbourne",
     image: "assets/races/australia.jpg",
     sessions: {
-      FP1: "2026-03-06T02:30:00",
-      FP2: "2026-03-06T06:00:00",
-      FP3: "2026-03-07T02:30:00",
-      Qualifying: "2026-03-07T06:00:00",
-      Race: "2026-03-08T04:00:00"
-    }
-  },
-  {
-    id: "bahrain",
-    name: "Grande Prémio do Bahrain",
-    circuit: "Sakhir",
-    image: "assets/races/bahrain.jpg",
-    sessions: {
-      FP1: "2026-04-10T11:30:00",
-      FP2: "2026-04-10T15:00:00",
-      FP3: "2026-04-11T12:30:00",
-      Qualifying: "2026-04-11T16:00:00",
-      Race: "2026-04-12T15:00:00"
-    }
+      FP1: "06/03/2026 02:30",
+      FP2: "06/03/2026 06:00",
+      FP3: "07/03/2026 02:30",
+      Qualificação: "07/03/2026 06:00",
+      Corrida: "08/03/2026 04:00"
+    },
+    raceDateISO: "2026-03-08T04:00:00"
   }
-  // Adicionar todas as corridas 2026 restantes
 ];
