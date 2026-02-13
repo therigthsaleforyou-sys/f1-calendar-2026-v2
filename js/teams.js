919bb17b729a20824919bb346ebbb0c8e02cb73d
therigthsaleforyou-sys <the.rigth.sale.for.you@gmail.com>
2026-02-13 17:15:54 +0000

    Update teams.js

--- js/teams.js
+++ js/teams.js
@@ -2,38 +2,63 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  const teamsData = [
-    {
-      name: "McLaren",
-      image: "assets/teams/mclaren.png",
-      drivers: ["Lando Norris", "Oscar Piastri"]
-    },
-    {
-      name: "Ferrari",
-      image: "assets/teams/ferrari.png",
-      drivers: ["Charles Leclerc", "Lewis Hamilton"]
-    },
-    {
-      name: "Red Bull Racing",
-      image: "assets/teams/redbull.png",
-      drivers: ["Max Verstappen", "Isack Hadjar"]
-    },
-    {
-      name: "Mercedes",
-      image: "assets/teams/mercedes.png",
-      drivers: ["George Russell", "Kimi Antonelli"]
-    },
-    {
-      name: "Aston Martin",
-      image: "assets/teams/astonmartin.png",
-      drivers: ["Fernando Alonso", "Lance Stroll"]
-    },
-    {
-      name: "Williams",
-      image: "assets/teams/williams.png",
-      drivers: ["Alexander Albon", "Carlos Sainz Jr."]
-    }
-  ];
+const teamsData = [
+  {
+    name: "McLaren",
+    image: "assets/teams/mclaren.png",
+    drivers: ["Lando Norris", "Oscar Piastri"]
+  },
+  {
+    name: "Ferrari",
+    image: "assets/teams/ferrari.png",
+    drivers: ["Charles Leclerc", "Lewis Hamilton"]
+  },
+  {
+    name: "Red Bull Racing",
+    image: "assets/teams/redbull.png",
+    drivers: ["Max Verstappen", "Isack Hadjar"]
+  },
+  {
+    name: "Mercedes",
+    image: "assets/teams/mercedes.png",
+    drivers: ["George Russell", "Kimi Antonelli"]
+  },
+  {
+    name: "Aston Martin",
+    image: "assets/teams/astonmartin.png",
+    drivers: ["Fernando Alonso", "Lance Stroll"]
+  },
+  {
+    name: "Williams",
+    image: "assets/teams/williams.png",
+    drivers: ["Alexander Albon", "Carlos Sainz Jr."]
+  },
+  {
+    name: "Alpine",
+    image: "assets/teams/alpine.png",
+    drivers: ["Pierre Gasly", "Franco Colapinto"]
+  },
+  {
+    name: "Haas",
+    image: "assets/teams/haas.png",
+    drivers: ["Esteban Ocon", "Oliver Bearman"]
+  },
+  {
+    name: "Racing Bulls",
+    image: "assets/teams/racing-bulls.png",
+    drivers: ["Liam Lawson", "Arvid Lindblad"]
+  },
+  {
+    name: "Audi F1 Team",
+    image: "assets/teams/audi.png",
+    drivers: ["Nico Hülkenberg", "Gabriel Bortoleto"]
+  },
+  {
+    name: "Cadillac F1 Team",
+    image: "assets/teams/cadillac.png",
+    drivers: ["Sergio Pérez", "Valtteri Bottas"]
+  }
+];
 
   const container = document.getElementById("teams-container");
   const backToTop = document.getElementById("back-to-top");
